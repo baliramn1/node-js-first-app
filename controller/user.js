0e1f647b6bdb15f30791402a96b0ad6ff37cdd93
baliramn1 <baliramnaik406@gmail.com>
2023-09-22 04:19:28 -0700

    t

--- controller/user.js
+++ controller/user.js
@@ -57,20 +57,20 @@ export const getMyProfile = async (req, res) => {
 
 
 
-export const logout = (req, res) => {
-    res.status(200).cookie(
-        "token",
-        "",
-        {
-            expires: new Date(Date.now()),
-            sameSite: process.env.NODE_ENV === "Developement" ? "lax" : "none", // It specifies that the site url(ports) are diff (for front and backend)
-            secure: process.env.NODE_ENV === "Developement" ? false : true,
-        })
-        .json({
-            success: true,
-            user: "loged out successfully",
-        })
-}
+// export const logout = (req, res) => {
+//     res.status(200).cookie(
+//         "token",
+//         "",
+//         {
+//             expires: new Date(Date.now()),
+//             sameSite: process.env.NODE_ENV === "Developement" ? "lax" : "none", // It specifies that the site url(ports) are diff (for front and backend)
+//             secure: process.env.NODE_ENV === "Developement" ? false : true,
+//         })
+//         .json({
+//             success: true,
+//             user: "loged out successfully",
+//         })
+// }
 
 
 
