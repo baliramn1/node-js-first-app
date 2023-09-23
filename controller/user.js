472a3184ea1e45cc37d82b15ec6aeef8572069e3
baliramn1 <baliramnaik406@gmail.com>
2023-09-23 03:20:46 -0700

    test3

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,7 @@
 import { user } from "../models/user.js"
 import bcrypt from "bcrypt"
 import { sendCookie } from "../utils/features.js"
+import ErrorHandler from "../middlewares/error.js"
 
 export const getAllUsers = async (req, res) => { }
 
